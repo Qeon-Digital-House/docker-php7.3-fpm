e6dd4414cd6d67940f4b387168e5218fc62c793c
qeon <sora@qeon.co.id>
2022-08-31 15:45:39 +0700

    Return failure on bad environment size.

--- runnables/90-fetch_envs.sh
+++ runnables/90-fetch_envs.sh
@@ -8,16 +8,16 @@ if [ -z "$USE_ETCD" ]; then
 	exit 0;
 fi
 
-[ -z "$ETCD_SERVER" ] && { >&2 echo "No etcd endpoint specified. Set etcd endpoints with ETCD_SERVER environment variable."; exit 1; };
-[ -z "$ENV_PATH" ] && { >&2 echo "No env file path set. Set env file path with ENV_PATH environment variable."; exit 1; };
-[ -z "$ENV_PREFIX" ] && { >&2 echo "No env prefix set. Set env prefix path with ENV_PREFIX environment variable."; exit 1; };
+[ -z "$ETCD_SERVER" ] && { >&2 echo "[!] FATAL: No etcd endpoint specified. Set etcd endpoints with ETCD_SERVER environment variable."; exit 1; };
+[ -z "$ENV_PATH" ] && { >&2 echo "[!] FATAL: No env file path set. Set env file path with ENV_PATH environment variable."; exit 1; };
+[ -z "$ENV_PREFIX" ] && { >&2 echo "[!] FATAL: No env prefix set. Set env prefix path with ENV_PREFIX environment variable."; exit 1; };
 
 # check if we have any etcd data to avoid bad env file.
 ENV_SIZE=$(ETCDCTL_API=3 etcdctl --endpoints "$ETCD_SERVER" get --prefix "$ENV_PREFIX" | wc -l)
 
 if [ "$ENV_SIZE" -eq "0" ]; then
-    >&2 echo "Can't get env data from etcd. Skipping."
-    exit 1
+    >&2 echo "[!] FATAL: Can't get env data from etcd. Skipping."
+    exit 1;
 fi
 
 # really fetch the entries from etcd.
@@ -32,3 +32,5 @@ ETCDCTL_API=3 \
 		KEY_PARSE=$(echo -ne "$KEY"); \
 		VALUE_PARSE=$(echo -ne "$VALUE"); \
 		echo ${KEY_PARSE##*/}="${VALUE_PARSE}"; done > "$ENV_PATH"
+
+exit 0
